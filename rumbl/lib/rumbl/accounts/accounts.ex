8e06a5070dc48834a4e6e3945917733830abc581
pragatech <pragatheswaran.a.c@hotmail.com>
2019-12-08 18:59:19 +0530

    "till 4th chapter - before Building forms"

---
+++ rumbl/lib/rumbl/accounts/accounts.ex
@@ -0,0 +1,23 @@
+defmodule Rumbl.Accounts do
+    @moduledoc """
+        The Accounts context
+    """
+    alias Rumbl.Repo
+    alias Rumbl.Accounts.User
+
+    def get_user(id) do
+        Repo.get(User, 1)
+    end
+
+    def get_user!(id) do
+        Repo.get!(User, 1)
+    end
+
+    def get_user_by(params) do
+       Repo.get_user_by(User, params)
+    end
+
+    def list_users() do
+        Repo.all(User)
+    end
+end
